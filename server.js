c16785008cb6188635786b66b1c9bed3fbcafb43
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-05-06 01:38:26 +0200

    Express Static Files

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+app = express();
+app.use(express.static('assets'));
+
+app.get('/', function(req,res) {
+    res.sendFile('/index.html');
+});
+app.get('/useform', function(req,res){
+    const response = {
+        first_name: req.query.first_name,
+        last_name:req.query.last_name
+    };
+    res.end(JSON.stringify(response));
+});
+var server = app.listen(3000, 'localhost', function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('App listening on http://' + host + ':' + port);
+});
